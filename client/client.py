7388f18c761a4d7eb0f518ebc9cc65d18b8be9ee
haticeadiguzel <adiguzell.hatice@gmail.com>
2023-10-05 10:20:01 +0300

    first commit

---
+++ client/client.py
@@ -0,0 +1,19 @@
+import socket
+
+server_address = ('localhost', 54632)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+
+while True:
+    calculation = input("Enter calculation or exit: ")
+
+    if calculation == "exit":
+        break
+
+    client_socket.send(calculation.encode())
+
+    result = client_socket.recv(1024).decode()
+    print(f"Sonuç: {result}")
+
+client_socket.close()
---
+++ server/server.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+import re
+
+def thread_client(client_socket):
+    try:
+        while True:
+            data = client_socket.recv(1024).decode()
+
+            if not data:
+                break
+
+            match = re.match(r'^([0-9]+)([+\-*/])([0-9]+)$', data)
+            if match:
+                num1, operator, num2 = match.groups()
+                num1, num2 = int(num1), int(num2)
+                if operator == '+':
+                    result = num1 + num2
+                elif operator == '-':
+                    result = num1 - num2
+                elif operator == '*':
+                    result = num1 * num2
+                elif operator == '/':
+                    if num2 != 0:
+                        result = num1 / num2
+                    else:
+                        client_socket.send("Zero division error".encode())
+                        continue
+                else:
+                    client_socket.send("Invalid operator".encode())
+                    continue
+                
+                client_socket.send(str(result).encode())
+            else:
+                client_socket.send("Invalid operation".encode())
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 54632)
+    server_socket.bind(server_address)
+    server_socket.listen(5)
+    print("Server is listening...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"{client_address} connected.")
+
+        client_handler = threading.Thread(target=thread_client, args=(client_socket,))
+        client_handler.start()
+
+if __name__ == "__main__":
+    main()
